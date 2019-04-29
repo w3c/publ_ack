83fdad95fd954a29e29a9053820a5fafc7ba821e
Ivan Herman <ivan@ivan-herman.net>
2019-04-29 14:18:14 +0200

    The mechanism works except for output management

--- main.js
+++ main.js
@@ -3,15 +3,17 @@
 'use strict';
 
 const { get_configuration } = require('./lib/get_config');
+const { create_ack_section } = require('./lib/get_data');
 
 const debug = true;
 
 async function main() {
     try {
         const config = await get_configuration();
-        if (config) {
-            if (debug) console.log(JSON.stringify(config, null, 4));
-        }
+        await create_ack_section(config);
+        // if (config) {
+        //     if (debug) console.log(JSON.stringify(config, null, 4));
+        // }
     } catch (e) {
         console.error(`${e.toString()}`);
         process.exit();
